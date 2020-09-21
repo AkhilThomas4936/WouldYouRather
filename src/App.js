ad0d6a50aef0c9ac7a75c099c3f85da3a2c40dde
AkhilThomas4936 <thomas.del011@gmail.com>
2020-09-21 18:31:33 -0400

    changes in routes and redux logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import { Route } from "react-router-dom";
+import React, { Component, Fragment } from "react";
+import { Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+
 import HomePage from "../src/components/HomePage";
+import SignIn from "../src/components/SignIn";
+import NotFound from "../src/components/NotFound";
 import QuestionDetailed from "../src/components/QuestionDetailed";
 import QuestionResult from "../src/components/QuestionResult";
 
-import React from "react";
+import { handleInitialData } from "./actions/shared";
 
-export default function App() {
-  return (
-    <div>
-      <Route exact path="/" render={() => <HomePage />} />
-      <Route
-        exact
-        path="/questions/:questionId"
-        render={(props) => <QuestionDetailed {...props} />}
-      />
-      <Route
-        exact
-        path="/questions/questionResult"
-        render={() => <QuestionResult />}
-      />
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.handleInitialData();
+  }
+  render() {
+    return (
+      <Switch>
+        {this.props.loading ? (
+          <Route exact path="/" render={() => <SignIn />} />
+        ) : (
+          <Fragment>
+            <Route exact path="/" render={() => <HomePage />} />
+            <Route
+              exact
+              path="/questions/:questionId"
+              render={(props) => <QuestionDetailed {...props} />}
+            />
+            <Route
+              exact
+              path="/questions/questionResult"
+              render={() => <QuestionResult />}
+            />
+          </Fragment>
+        )}
+        {/* <Route render={() => <NotFound />} /> */}
+      </Switch>
+    );
+  }
 }
+
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null,
+  };
+}
+
+export default connect(mapStateToProps, { handleInitialData })(App);
--- src/components/Signin.js
+++ src/components/Signin.js
@@ -3,6 +3,10 @@ import { connect } from "react-redux";
 import { setAuthedUser } from "../actions/authedUser";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "../components/ui/theme";
+
+import Navbar from "../components/Navbar";
 
 import welcome from "../utils/images/welcome.svg";
 
@@ -21,43 +25,46 @@ class SignInTest extends Component {
   render() {
     console.log(this.state.authedId);
     return (
-      <Paper className="signin-container" elevation="24">
-        <div>
-          <div style={{ fontFamily: "Helvetica Neue" }} className="welcome">
-            <h1>Welcome to the Would You Rather App!</h1>
-            <h3>Please sign in to continue</h3>
+      <ThemeProvider theme={theme}>
+        <Navbar />
+        <Paper className="signin-container" elevation="24">
+          <div>
+            <div style={{ fontFamily: "Helvetica Neue" }} className="welcome">
+              <h1>Welcome to the Would You Rather App!</h1>
+              <h3>Please sign in to continue</h3>
+            </div>
+            <img className="logo" src={welcome} alt="logo" />
+            <h1 className="signin">Sign in</h1>
+
+            <form className="form">
+              <select
+                onChange={(e) => this.handleChange(e)}
+                placeholder="Select User"
+              >
+                <option value="null">Select User</option>
+                {Object.keys(this.props.users).map((user) => (
+                  <option key={user} value={this.props.users[user].id}>
+                    {this.props.users[user].name}
+
+                    {/* <img src={this.props.users[user].avatar} /> */}
+                  </option>
+                ))}
+              </select>
+              <Button
+                style={{
+                  width: "25%",
+                  marginTop: "1.2em",
+                }}
+                variant="contained"
+                color="secondary"
+                onClick={this.onClick}
+              >
+                Signin
+              </Button>
+            </form>
           </div>
-          <img className="logo" src={welcome} alt="logo" />
-          <h1 className="signin">Sign in</h1>
-
-          <form className="form">
-            <select
-              onChange={(e) => this.handleChange(e)}
-              placeholder="Select User"
-            >
-              <option value="null">Select User</option>
-              {Object.keys(this.props.users).map((user) => (
-                <option key={user} value={this.props.users[user].id}>
-                  {this.props.users[user].name}
-
-                  {/* <img src={this.props.users[user].avatar} /> */}
-                </option>
-              ))}
-            </select>
-            <Button
-              style={{
-                width: "25%",
-                marginTop: "1.2em",
-              }}
-              variant="contained"
-              color="secondary"
-              onClick={this.onClick}
-            >
-              Signin
-            </Button>
-          </form>
-        </div>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     );
   }
 }
